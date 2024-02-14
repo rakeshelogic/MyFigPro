2a2f3c81c8ec26271c85d35fa24bdc18d8ba6046
BrunoCat118 <github_out1@outlook.com>
2024-02-14 10:49:56 +0530

    Add Live component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Live from "@/components/Live";
 import { Room } from "./Room";
 
 export default function Home() {
-  return <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>;
+  return (
+    <div>
+      <Live />
+    </div>
+  );
 }

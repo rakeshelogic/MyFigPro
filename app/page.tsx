d74eb4184d28dfe2a36a90bd170132757952471a
BrunoCat118 <github_out1@outlook.com>
2024-02-18 18:22:27 +0530

    feat: Add canvas functionality and sidebar components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,54 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
+
+import LeftSideBar from "@/components/LeftSideBar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSideBar from "@/components/RightSideBar";
 
-export default function Home() {
+function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>("rectangle");
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+
+    window.addEventListener("resize", () => {
+      handleResize({ canvas: fabricRef.current });
+    });
+  }, []);
+
   return (
     <main className="h-[100dvh] overflow-hidden">
       <Navbar />
 
-      <section className="flex h-[100dvh] flex-row">
-        <Live />
+      <section className="flex h-full flex-row">
+        <LeftSideBar />
+        <Live canvasRef={canvasRef} />
+        <RightSideBar />
       </section>
     </main>
   );
 }
+
+export default Page;

69fa362bd9e03b90be156c80b87eecad65430c02
BrunoCat118 <github_out1@outlook.com>
2024-02-18 18:21:17 +0530

    refact: Add experimental features and webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }];
+    return config;
+  },
   images: {
     remotePatterns: [
       {

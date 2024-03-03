c348215027d28d495afa9978b5a5318dc6126868
BrunoCat118 <github_out1@outlook.com>
2024-03-03 22:31:21 +0530

    refactor: RightSideBar component to include settings for dimensions, text, color, and export

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,7 +1,52 @@
-function RightSideBar() {
+import { RightSidebarProps } from "@/types/type";
+import Color from "./settings/Color";
+import Dimensions from "./settings/Dimensions";
+import Export from "./settings/Export";
+import Text from "./settings/Text";
+import { modifyShape } from "@/lib/shapes";
+
+function RightSideBar({
+  elementAttributes,
+  setElementAttributes,
+  fabricRef,
+  activeObjectRef,
+  isEditingRef,
+  syncShapeInStorage,
+}: RightSidebarProps) {
+  const handleInputChange = (property: string, value: any) => {
+    if (!isEditingRef.current) isEditingRef.current = true;
+
+    setElementAttributes((prev) => ({
+      ...prev,
+      [property]: value,
+    }));
+
+    modifyShape({
+      canvas: fabricRef.current as fabric.Canvas,
+      property,
+      value,
+      activeObjectRef,
+      syncShapeInStorage,
+    });
+  };
+
   return (
     <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none">
-      <h3 className="px-5 pt-4 text-xs">Design</h3>
+      <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+        Makes changes to canvas as you like
+      </span>
+
+      <Dimensions
+        width={elementAttributes.width}
+        height={elementAttributes.height}
+        handleInputChange={handleInputChange}
+        isEditingRef={isEditingRef}
+      />
+      <Text />
+      <Color />
+      <Color />
+      <Export />
     </section>
   );
 }
